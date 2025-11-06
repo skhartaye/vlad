f4ae126e702ee7fef4a8014f94f27e6663e0791d
Aeroband User <user@example.com>
2025-11-06 22:55:48 +0800

    Add database connection timeout settings for Wasmer

--- classes/Database.php
+++ classes/Database.php
@@ -36,7 +36,10 @@ class Database {
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
-                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES " . $this->charset
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES " . $this->charset,
+                PDO::ATTR_TIMEOUT => 10,
+                PDO::ATTR_PERSISTENT => false,
+                PDO::MYSQL_ATTR_CONNECT_TIMEOUT => 10
             ];
 
             $this->conn = new PDO($dsn, $this->username, $this->password, $options);
