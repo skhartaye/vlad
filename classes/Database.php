63cf28b2b435aa332909fd76a5ee6953ba7a1dd4
Aeroband User <user@example.com>
2025-11-06 23:04:01 +0800

    Fix PDO constants compatibility for Wasmer PHP

--- classes/Database.php
+++ classes/Database.php
@@ -36,11 +36,17 @@ class Database {
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
-                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES " . $this->charset,
                 PDO::ATTR_TIMEOUT => 10,
-                PDO::ATTR_PERSISTENT => false,
-                PDO::MYSQL_ATTR_CONNECT_TIMEOUT => 10
+                PDO::ATTR_PERSISTENT => false
             ];
+            
+            // Add MySQL-specific options if available
+            if (defined('PDO::MYSQL_ATTR_INIT_COMMAND')) {
+                $options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES " . $this->charset;
+            }
+            if (defined('PDO::MYSQL_ATTR_CONNECT_TIMEOUT')) {
+                $options[PDO::MYSQL_ATTR_CONNECT_TIMEOUT] = 10;
+            }
 
             $this->conn = new PDO($dsn, $this->username, $this->password, $options);
             
